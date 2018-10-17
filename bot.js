ec931974dc63c01e33dcdfc70e45bcac9984315f
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-10-17 14:32:21 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,87 +1,121 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client(); 
+const config = require("./config.json"); 
 
 
-bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: `e mandando meu criador tomar no ##`, type: 1, url: 'https://www.youtube.com/yRecky'} });
-    console.log('Logado');
-});
-bot.on('guildMemberAdd', member => {
-    const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
-   
-    let canal = member.guild.channels.find(`name`, "bem-vindo-📣");
-    if (!canal) return;
-  
-    var embed = new Discord.RichEmbed()
-    .setColor(randomColor)
-    .setDescription(`🎈 **|** ${member} **Seja bem-vindo(a) ao nosso servidor.**`)
-    canal.send({embed : embed})
-  });
 
-bot.on('message', message => {
-    if (message.content.startsWith('/modd')){
-        message.channel.send('http://www.modd.io/');
-    }
-    if (message.content.startsWith('/canal')){
-        if(message.channel.id !== '500821240869814272') return message.channel.send('você não pode usar comandos aqui, vai em <#500821240869814272> e digite o comando!');
-        message.channel.send('https://www.youtube.com/channel/UCNx4oMYW8osf3ea2wfEd5pg');
-    }
-    if (message.content.startsWith('/beijar')){
-        let user = message.mentions.users.first();
-        if(message.channel.id !== '500821240869814272') return message.channel.send('você não pode usar comandos aqui, vai em <#500821240869814272> e digite o comando!');
-        if(!user) return message.channel.send('Você precisa mencionar alguém para beijar!');
-        let gifs = ['https://media.giphy.com/media/1BcfiGlOGXzQ5xU4DA/giphy.gif'] 
-        let random = Math.round(Math.random() * gifs.length);
-        let embed = new Discord.RichEmbed()
-            .setTitle(`${message.author.username} deu um beijo no(a) ${user.username}`)
-            .setImage(gifs[gifs.length == 1 ? 0 : random == 0 ? random + 1 : random - 1])
-            .setColor('#f2f1f2')
-            .setTimestamp()
-            message.channel.send(embed);
-         }
-    if (message.content.startsWith('/roleta')){
-        if(message.channel.id !== '500821240869814272') return message.channel.send('você não pode usar comandos aqui, vai em <#500821240869814272> e digite o comando!');
-        let a = ['Voce foi execultado! a bala foi certeira.', 'Voce teve sorte! a bala nao foi disparada.']
-        let b = Math.floor(Math.random() * a.length)
-         message.reply(a[b])
-          }
-});
-bot.on('message', message => {
-    let arraymsg = message.content.split(" ");
-let cmd = arraymsg[0].toLowerCase()
-let args = message.content.split(" ").slice(1);
-if(cmd === '/anuncio'){
-    if(!message.member.hasPermissions("BAN_MEMBERS")) return message.reply("Você não tem permissão para usar este comando.")
-    const args = message.content.split(" ").slice(1);
-    const prefix = '/'
-    message.delete()
-    if (!args.slice(0).join(' ')) return message.channel.send('test')
-    message.channel.send({embed:{
-        'description':args.slice(0).join(' ')
-        ,'color':message.member.highestRole.color,
-        "thumbnail":{
-            }
+client.on('ready', () =>{
+    let status = [
+        {name: 'Ajuda?│/ajuda', type: 'STREAMING', url: 'https://twitch.tv/srmisterii'},
+        {name: 'Braains.io', type: 'LISTENING'},
+        {name: '😉Steam😉', type: 'PLAYING'},
+        {name: 'NitrooPVP│YouTube', type: 'WATCHING'},
+      ];
+      
+      //STREAMING = Transmitindo
+      //LISTENING = Ouvindo
+      //PLAYING = Jogando
+      //WATCHING = Assistindo
+      
+        function setStatus() {
+            let randomStatus = status[Math.floor(Math.random() * status.length)];
+            client.user.setPresence({game: randomStatus});
         }
-    }
-    )
-}
-});
-bot.on('message', message => {
-    let arraymsg = message.content.split(" ");
-let cmd = arraymsg[0].toLowerCase()
-let args = message.content.split(" ").slice(1)
-    if(cmd === '/ban'){
-        const args = message.content.split(" ").slice(1);
-        var razao = args.slice(1).join(" ")
-            var membro = message.mentions.members.first();
-            if(!message.member.hasPermissions("BAN_MEMBERS")) return message.reply("Você não tem permissão para usar este comando.")
-            if(!membro) return message.reply("Você não mencionou ninguém.")
-            if(!membro.bannable) return message.reply("Você não pode banir essa pessoa.")
-            if(razao.length < 1) return message.reply("Coloque um motivo!")
-            membro.ban()
-            message.channel.send(`O membro ${membro.user.username} foi banido do servidor.\nMotivo: ${razao}`)
-      }
+      
+        setStatus();
+        setInterval(() => setStatus(), 10000);  //10000 = 10Ms = 10 segundos
 });
 
-// THIS  MUST  BE  THIS  WAY
-bot.login(process.env.BOT_TOKEN);
+client.on("guildCreate", guild => {
+  console.log(`O Nighty BOT entrou no servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
+  client.user.setActivity(`Estou em ${client.guilds.size} servidores.`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`O Nighty BOT foi removido do servidor: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Estou em ${client.guilds.size} servidores.`);
+});
+
+
+client.on("message", async message => {
+  let responseObject = {
+    "oi" : "Olá, como você está hoje?",
+    "bem" : "Que bom que você está bem :slight_smile:"
+  };
+  
+  if (responseObject[message.content]){
+    message.channel.send(responseObject[message.content]);
+  }
+
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    if(!message.content.startsWith(config.prefix)) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const comando = args.shift().toLowerCase();
+  
+  if(comando === "ping") {
+    const m = await message.channel.send("Ping?");
+    message.delete().catch(O_o=>{});
+    m.edit(`${message.member}, Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms.`);
+  }
+  if(comando === "apagar") {
+    if(!message.member.hasPermissions("BAN_MEMBERS")) return message.reply("você não tem permissão de usar esse comando")
+    const deleteCount = parseInt(args[0], 10);
+    message.delete().catch(O_o=>{});
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Coloque ou forneça um número entre 2 e 100 para remover as mensagens!");
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Não foi possível deletar mensagens devido a: ${error}`));
+  }
+  if(comando === "aviso") { 
+    if(!message.member.hasPermissions("BAN_MEMBERS")) return message.reply("você não tem permissão de usar esse comando")
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{});
+    message.channel.send("@everyone\n\n"+ sayMessage);
+  }
+  if(comando === "urgente") { 
+    if(!message.member.hasPermissions("BAN_MEMBERS")) return message.reply("você não tem permissão de usar esse comando")
+    const sayMessage = args.join(" ");
+    message.delete().catch(O_o=>{});
+    message.channel.send("@here\n\n"+ sayMessage);
+  }
+  if(comando === "ajuda") {
+    const deleteCount = parseInt(args[0], 10);
+    message.delete().catch(O_o=>{});
+    message.reply("\n\nComandos do BOT:\n\nAdministradores:\n- !apagar <2 a 100> - Apagar as mensagens do chat.\n- !aviso <mensagem> - Avisar o servidor do discord.\n\nMembros:\n- !ping - Para ver seu ping/ms.\n- !criador - Para ver quem e meu Pai/Criador.");
+  }
+  if(comando === "mcskin"){
+    let reason = args.slice(0).join(' ');
+      if (reason.length < 1) return message.reply('**Cite um nick de minecraft!**');
+      
+      let embed = new Discord.RichEmbed()
+    
+      .setTitle(`:minecraftdirt: ${args[0]}`)
+    .setImage(`https://mc-heads.net/skin/${args[0]}`)
+     .setFooter(message.author.tag,message.author.avatarURL )
+      .setTimestamp(new Date())
+      .setColor('#00BFFF')
+      message.channel.send(embed)
+    }
+    if(comando === `humano`) {
+        if(message.author.bot) return;
+        if(message.channel.id === "465310587719516180") {
+            if(message.content === "/humano") message.member.addRole("502167468702433280")
+         message.channel.send('Humano!')
+        }
+        
+        }
+    if(comando === `zombie`) {
+        if(message.author.bot) return;
+        if(message.channel.id === "465310587719516180") {
+            if(message.content === "/zombie") message.member.addRole("502167548977217566")
+         message.channel.send('Zombi!')
+        }
+            
+        }
+});
+
+client.login(config.token);
