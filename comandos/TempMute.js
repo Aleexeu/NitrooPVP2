83a9a1df652a53ab6f62bb97499dcfe4362ffec4
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-16 12:01:09 -0200

    Update TempMute.js

--- comandos/TempMute.js
+++ comandos/TempMute.js
@@ -28,7 +28,7 @@ var razao = args.slice(2).join(" ")
 
   let mutetime = args[1];
   if(!mutetime) return message.reply("Você não especificou um tempo!");
-  var canal = message.guild.channels.find("name", "🚫punições");
+  var canal = message.guild.channels.find("name", "punishments");
   if (!canal) return; canal.send({
 embed: { 
 description:`:mute: Membro Silenciado.\nUsuário mutado com sucesso!\n \n \n:beginner: Autor:\n<@${message.author.id}>\n \n:bust_in_silhouette:  Usuário:\n<@${tomute.id}>\n \n:clock3: Duração:\n${ms(ms(mutetime))}\n \n:pencil: Motivo:\n ${razao}`, 
